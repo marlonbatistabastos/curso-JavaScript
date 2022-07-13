0572cf0d310795b3b8fa3254d66c6b1e5ceaf6cc
marlonbatistabastos <opalbugado231@gmail.com>
2022-07-13 11:27:49 -0300

    como converter string para number

--- js/aula11.js
+++ js/aula11.js
@@ -1,1 +1,7 @@
-alert('mensagem')
+const num1 = prompt ('digite um numero')
+const num2 = prompt ('digite o segundo numero')
+const soma = parseInt(num1) + parseInt(num2)
+let resultado = alert('resultado e igual '+ soma
+
+
+ )
