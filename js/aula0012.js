cdc5918c6b56c0c98446dfcb94d44eab9901d824
marlonbatistabastos <opalbugado231@gmail.com>
2022-07-13 16:25:52 -0300

    dizima periodica

---
+++ js/aula0012.js
@@ -0,0 +1,11 @@
+let varA = 'A' //B
+let varB = 'B' //C
+let varC = 'C' //A
+var var4 = 'A'
+
+var4 = varA
+varA = varB
+varB = varC
+varC = var4
+
+console.log(varA ,varB, varC)
--- js/aula11.js
+++ js/aula11.js
@@ -1,7 +1,4 @@
 const num1 = prompt ('digite um numero')
 const num2 = prompt ('digite o segundo numero')
 const soma = parseInt(num1) + parseInt(num2)
-let resultado = alert('resultado e igual '+ soma
-
-
- )
+let resultado = alert('resultado e igual '+ soma)
