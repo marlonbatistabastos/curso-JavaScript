02ff583778894b5f55963e3b7463f4da761d79dc
marlonbatistabastos <opalbugado231@gmail.com>
2022-07-18 21:52:49 -0300

    aprendendo arrays

---
+++ array/index.js
@@ -0,0 +1,48 @@
+
+const marlom = {
+    nome: "marlom",
+    idade: 19,
+    sexo: "masculino"
+}
+const fulano = {
+    nome: "marlom",
+    idade: 19,
+    sexo: "masculino"
+}
+
+const listaFuncionarios = [
+    marlom,
+    marlom,
+    marlom
+];
+
+const robson= {
+    nome: "robson",
+    sexo:"masculino",
+    idade:"45 anos"
+
+}
+const osmar= {
+
+    nome:"osmar",
+    sexo:"nao binario",
+    idade:"24"
+}
+
+const juliana={
+
+nome: "juliana",
+sexo: "feminino",
+idade:"69" }
+
+
+const Listadegerentes =[
+    listaFuncionarios,
+    juliana,
+    osmar,
+    robson
+]
+
+
+
+console.log(Listadegerentes)
---
+++ array/index2.js
@@ -0,0 +1,36 @@
+const curso01 = {
+    nome: "cursojavascript",
+    autor: "professorguanabara",
+}
+const curso02 = {
+    nome: "cursoaleatorio",
+    autor: "mestrejedi"
+}
+const curso03 = {
+    nome: "cursoaleatorio",
+    autor: "professorAleatorio"
+}
+
+
+const fulano = {
+    nome: "fulano",
+    idade: 20,
+    cursos: curso01
+}
+const cicrano = {
+    nome: "cicrano",
+    idade: 36,
+    cursos: curso02
+}
+const deutrano = {
+    nome: "deutrano",
+    idade: 84,
+    cursos: curso03
+}
+
+const listaUS = [
+    deutrano,cicrano,deutrano
+]
+
+
+console.log(listaUS)
